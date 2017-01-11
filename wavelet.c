b5585f641af237220c1b836b322cf6eaeac95498
Andrew Hoekstra <connect@andrewhoekstra.com>
2017-01-10 21:51:04 -0600

    initial commit

---
+++ wavelet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "wavelet.h"
+
+int test(const char *string) {
+    
+    printf("Hello ");
+    printf("%s", string);
+    printf("!\n");
+    
+    return 0;
+    
+}
---
+++ waveletTest.c
@@ -0,0 +1,7 @@
+#include "wavelet.h"
+
+int main(int argc, char *argv[]) {
+    test("Andrew");
+    
+    return 0;
+}
